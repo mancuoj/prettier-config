ccac7c75aa93f26a17d6b71281ca281933444937
mancuoj <mancuoj@gmail.com>
2024-03-31 13:14:18 +0800

    fix: wrong env cannot run

--- cli.js
+++ cli.js
@@ -6,8 +6,9 @@ import fsp from 'node:fs/promises'
 import process from 'node:process'
 import c from 'picocolors'
 
-async function ensurePackages(packages) {
-  const missingPkgs = packages.filter((p) => !isPackageExists(p))
+async function ensurePackages(pkgs) {
+  if (process.env.CI ?? !process.stdout.isTTY) return
+  const missingPkgs = pkgs.filter((p) => !isPackageExists(p))
   if (missingPkgs.length > 0) {
     console.log(c.cyan(`Installing required packages: ${missingPkgs.join(', ')}`))
     await installPackage(missingPkgs, { dev: true })
